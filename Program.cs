97925f94a589d7dcba609c9e168947c8f19d8a64
Saar Shen <xiaomi7732@hotmail.com>
2021-04-23 14:12:52 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LearnAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test aa = new Test();
+            
+        }
+    }
+
+    public class Test : IDisposable
+    {
+        public void Dispose()
+        {
+            
+        }
+    }
+
+    public enum TestEnumNoZero
+    {
+        Unknown = 1
+    }
+}
